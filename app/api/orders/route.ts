3c5a5a85734d32ce88a368d809be980920af8f98
tunngc0941 <tuanngoc0906@gmail.com>
2026-03-26 22:29:12 +0700

    get products && post orders

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -28,3 +28,49 @@ export async function GET() {
         );
     }
 }
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const { product_id, quantity } = body;
+
+        if (!product_id || !quantity) {
+            return NextResponse.json(
+                { success: false, message: "Thiếu product_id hoặc quantity" },
+                { status: 400 }
+            );
+        }
+        
+        const productResult = await conn`
+        SELECT price FROM products WHERE id = ${product_id}
+        `;
+
+        if (productResult.length === 0) {
+            return NextResponse.json(
+                { success: false, message: "Sản phẩm không tồn tại" },
+                { status: 404 }
+            );
+        }
+
+        const price = productResult[0].price;
+        const total_price = price * quantity;
+
+        // Tạo đơn hàng
+        const result = await conn`
+        INSERT INTO orders (product_id, quantity, total_price)
+        VALUES (${product_id}, ${quantity}, ${total_price})
+        RETURNING id, product_id, quantity, total_price, created_at
+        `;
+
+        return NextResponse.json(
+            { success: true, order: result[0] },
+            { status: 201 }
+        );
+    } catch (error: unknown) {
+        console.error("Lỗi khi tạo đơn hàng", error);
+        return NextResponse.json(
+            { success: false, message: "Tạo đơn hàng thất bại", errorDetail: error },
+            { status: 500 }
+        );
+    }
+}
